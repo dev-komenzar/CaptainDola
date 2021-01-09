4b046c43ba5af4c413786a1a221fcd0ddece2438
kometan MacBook <takuya.kometan@gmail.com>
2021-01-09 18:44:27 +0900

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,90 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <p>テスト</p>
+    <div>
+      <input type="number" name="currentPrice" id="price" v-model.number="inputPrice">
+      <input type="number" name="unit" id="unit" v-model.number="unit">
+      <button @click="getPrice">Update</button>
+    </div>
+    <div>
+    <div class="container">
+      <div class="item">
+        <h2>0.1%刻み</h2>
+        <ul>
+          <li v-for="item in multipliedPrices" :key="item.index">
+            {{item.ratio}}% $ {{ item.price }} : {{ calcProfit(item.price)}}
+          </li>
+        </ul>
+      </div>
+      <div class="item">
+        <h2>差分（{{unit}}刻み）</h2>
+        <ul>
+          <li v-for="item in differencedPrices" :key="item.index">
+            $ {{ item.price}} : {{ calcProfit(item.price)}}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div></div>
   </div>
+  </div>
+  
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    
+  },
+  data: function(){
+    return{
+      inputPrice: 4126000,
+      unit: 0.01
+    }
+  },
+  computed:{
+    multipliedPrices: function(){
+      const arr = [-2,-1.66,-1.5,-1.33,-1,-0.9,-0.8,-0.7,-0,6,-0.5,-0.4,-0.3,-0.2,-0.1, 0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.33,1.5,1.66,2].reverse()
+      return arr.map((element, index) =>{
+        return {
+          price: Math.floor((100+element) * this.inputPrice /100),
+          id: index,
+          ratio: element
+        }
+      })
+    },
+    differencedPrices: function(){
+      const arr =[...Array(21).keys()].map(i => i - 10).reverse()
+      const differenceValue = 10000
+      return arr.map((element, index) =>{
+        return {
+          price: this.inputPrice + differenceValue*element,
+          id: index
+        }
+      })
+    },
+    
+  },
+  created(){
+    const url = '/v1/getboard'
+    axios.get(url).then(res =>{
+        this.inputPrice = res.data.mid_price
+      }).catch(err=>console.log(err))
+  },
+  methods:{
+    calcProfit: function(a){
+      return Math.floor((Number(a) - this.inputPrice)*this.unit)
+    },
+    getPrice(){
+      const url = '/v1/getboard'
+      axios.get(url).then(res =>{
+        this.inputPrice = res.data.mid_price
+      }).catch(err=>console.log(err))
+
+    }
   }
 }
 </script>
@@ -49,11 +98,13 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+
 a {
   color: #42b983;
+}
+
+.container {
+  display:flex;
+  justify-content: center;
 }
 </style>
